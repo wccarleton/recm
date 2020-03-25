29ca2b7f02e98b916ac8b4c95821cdaef3a718ac
wccarleton <ccarleton@protonmail.com>
2020-03-25 11:41:32 +0100

    a

--- R_scripts/Analyses/mcmc_diag_geweke.R
+++ R_scripts/Analyses/mcmc_diag_geweke.R
@@ -11,5 +11,19 @@ for(j in mcmc_results_files){
    geweke_z <- geweke.diag(mcmc_mat[burnin:niter,])$z
    print(j)
    print(geweke_z[which(geweke_z > 2.96)])
-   write.csv(geweke_z,file=paste("../Results/MCMC/Exp/geweke_",csv_file_name,".csv",sep=""))
+   write.csv(geweke_z,file=paste("../Results/MCMC/Geweke/geweke_",csv_file_name,".csv",sep=""))
+}
+
+#palaeoclimate function
+mcmc_results_files <- list.files("../Results/MCMC/Kennett/")
+
+for(j in mcmc_results_files){
+   csv_file_name <- strsplit(j,split=".RData")[[1]]
+   load(file=paste("../Results/MCMC/Kennett/",j,sep=""))
+   niter <- dim(samples$samples)[1]
+   mcmc_mat <- cbind(samples$samples,samples$samples2)
+   geweke_z <- geweke.diag(mcmc_mat[burnin:niter,])$z
+   print(j)
+   print(geweke_z[which(geweke_z > 2.96)])
+   write.csv(geweke_z,file=paste("../Results/MCMC/Geweke/geweke_",csv_file_name,".csv",sep=""))
 }
