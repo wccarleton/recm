a20f3ebaa48c437d747ce60612db967e601f44fd
wccarleton <ccarleton@protonmail.com>
2020-03-25 11:59:56 +0100

    file cleanup

--- R_scripts/Analyses/posterior_pred_pois_ar.R
+++
@@ -1,9 +0,0 @@
-posteriorPoisAR <- function(s,Xs,alphas){
-    cols <- colnames(s)
-    N <- length(alphas)
-    lambda <- exp(alphas + s[1] * Xs)
-    predY <- rpois(n=N,lambda=lambda)
-    return(predY)
-}
-
-#full_posterior_pred <- apply(samples[1000:niter,],1,posteriorPoisAR,Xs=X,alphas=grep("alpha\\[",colnames(samples)))
