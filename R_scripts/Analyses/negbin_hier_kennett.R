64cf0bb1f6b7e0b33a29de9df1255047132fc07b
wccarleton <ccarleton@protonmail.com>
2020-04-02 09:30:54 +0200

    analyses

--- R_scripts/Analyses/negbin_hier_kennett.R
+++ R_scripts/Analyses/negbin_hier_kennett.R
@@ -74,7 +74,7 @@ nbModelMCMC <- buildMCMC(nbModel_conf)
 C_nbModelMCMC <- compileNimble(nbModelMCMC,project=nbModel)
 
 #number of MCMC iterations
-niter=2000000
+niter=1000000
 
 #set seed for replicability
 set.seed(1)
